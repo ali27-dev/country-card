f42fd694988c5d5e2c9982f506d60a170311ecc8
Abid-ali <khanali3229@gmail.com>
2024-12-30 09:21:53 +0500

    i crate a function to create another card for neighbor and also i visible html  code

--- script.js
+++ script.js
@@ -9,6 +9,31 @@ const countriesContainer = document.querySelector('.countries');
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 ///////////////////////////////
+const renderCountry = function (data) {
+  const languages = Object.values(data.languages).join(', ');
+  const currency = Object.values(data.currencies)
+    .map(curr => curr.name)
+    .join(', ');
+
+  const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              data.population / 1000000
+            ).toFixed(1)} million</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currency}</p>
+          </div>
+        </article>
+      `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -17,8 +42,10 @@ const getCountryData = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText); // Use array destructuring
     console.log(data);
+
+    renderCountry(data);
   });
 };
 
 getCountryData('pakistan');
-getCountryData('usa');
+// getCountryData('usa');
