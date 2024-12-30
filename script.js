08fd052e3daf5bbf5f2ff9dfa381a31a2e7949fb
Abid-ali <khanali3229@gmail.com>
2024-12-30 09:12:04 +0500

    i send XMLHttpRequest to a server,i use  rest API in which  i use rest country API

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const countriesContainer = document.querySelector('.countries');
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 ///////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText); // Use array destructuring
+    console.log(data);
+  });
+};
+
+getCountryData('pakistan');
+getCountryData('usa');
